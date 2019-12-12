714aabdc85472584bde93c5fe5c24cfb40774584
virtualcursor <2540378-virtualcursor@users.noreply.gitlab.com>
2019-12-12 17:19:54 +0800

    Use babel.

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,45 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     devtool: 'source-map',
-    entry: './src/index.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'static'),
         filename: 'js/app.js'
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: {
-                        sourceMap: true,
-                    }
-                },
-                {
-                    loader: 'postcss-loader',
-                    options: {
-                        sourceMap: true,
-                    }
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        sourceMap: true,
-                    }
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
                 }
-            ]
-        }]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
